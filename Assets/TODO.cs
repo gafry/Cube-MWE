9f509be40208265c9ed31660177feb394ea496e8
gafry <gafry10@gmail.com>
2021-04-02 17:21:02 +0200

    Update TODO.cs
    Todo

--- Assets/TODO.cs
+++ Assets/TODO.cs
@@ -8,4 +8,6 @@ public class TODO : MonoBehaviour
     // Zbavit se tech bilych sracek na horami (mozna v blit shaderu)
     // Pridat lokalni svetla
     // Zkusit udelat 2 meshe pro chunk - jeden se svetelnymi kostkami a druhy s normalnimi
+    
+    // Na konci je potreba zmenit nastaveni Jobs - vypnout kontroly atd pro maximalni performance
 }
