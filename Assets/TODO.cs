91cb1db6b07aab6c0ef8fa3203b6e1f91d9f9274
gafry <gafry10@gmail.com>
2021-04-02 17:35:59 +0200

    Update TODO.cs
    todo

--- Assets/TODO.cs
+++ Assets/TODO.cs
@@ -10,4 +10,7 @@ public class TODO : MonoBehaviour
     // Zkusit udelat 2 meshe pro chunk - jeden se svetelnymi kostkami a druhy s normalnimi
     
     // Na konci je potreba zmenit nastaveni Jobs - vypnout kontroly atd pro maximalni performance
+    /* Pokud to nacitani chunku nebude stacit a bude dlouho trvat, jde jeste udelat, aby se prekryvalo
+       FinishcreatingChunk() s generovanim bloku, protoze dokoncovani chunku jede jen na hlavnim vlakne,
+       ale bloky se generuji jen na workerech*/
 }
