d42ea2d94806b7e5098115a5ec8283f0546cf721
gafry <gafry10@gmail.com>
2021-04-16 23:02:13 +0200

    Update TODO.cs

--- Assets/TODO.cs
+++ Assets/TODO.cs
@@ -4,19 +4,13 @@ using UnityEngine;
 
 public class TODO : MonoBehaviour
 {
-    // Zkusit vypnout reprojekci po nacteni chunku
     // Zbavit se tech bilych sracek na horami (mozna v blit shaderu)
     // Pridat lokalni svetla
     // Zkusit udelat 2 meshe pro chunk - jeden se svetelnymi kostkami a druhy s normalnimi
     
     // Na konci je potreba zmenit nastaveni Jobs - vypnout kontroly atd pro maximalni performance
-    /* Pokud to nacitani chunku nebude stacit a bude dlouho trvat, jde jeste udelat, aby se prekryvalo
-       FinishcreatingChunk() s generovanim bloku, protoze dokoncovani chunku jede jen na hlavnim vlakne,
-       ale bloky se generuji jen na workerech*/
        
     // GraphicsSettings.useScriptableRenderPipelineBatching = true;
-    
-    // rayIntersection.color = _Color * 0.5f * color;
     
     // # DXR Extra - Simple Lighting
     /*float factor = isShadowed ? 0.3 : 1.0;
